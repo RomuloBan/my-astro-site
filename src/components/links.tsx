31794088a3ca28a15a862e25313fac6fa3a47c77
romulo.banares <romulo88@hotmail.com>
2022-09-11 22:59:25 +0200

    add links styles

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,8 +1,25 @@
-const links = ['twitter', 'instagram', 'google'];
+import styles from './links.module.css';
+
+type Link = {
+  label: string;
+  url: string;
+};
+
+const LINKS: Link[] = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/romuloban'
+  },
+  {
+    label: 'GitHub',
+    url: 'https://github.com/romuloban'
+  },
+];
+
 export function Links() {
  return (
-   <div>
-   {links.map(l => <p>{l}</p>)}
-   </div>
+   <nav className={styles.links}>
+     {LINKS.map(link => <a key={link.url} href={link.url} class={styles.link}>{link.label}</a>)}
+   </nav>
  )
 }
